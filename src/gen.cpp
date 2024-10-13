4a569e3541e380cee4de52b6b0b00f33bba2c894
cvada <gfinfgin@gmail.com>
2024-10-13 19:41:04 +0200

    modositas

--- src/gen.cpp
+++ src/gen.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cmath>  // A sin függvényhez szükséges
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
@@ -24,12 +25,24 @@ private:
     {
         auto message_sin = std_msgs::msg::Float32();
         auto message_rnd = std_msgs::msg::Float32();
-        message_sin.data = sin(count_++ / 50.0) * 100;
+
+        // Transzformált szinusz függvény: A * sin(B * t) + C
+        // A: Amplitúdó, B: frekvencia változtatás, C: eltolás
+        float amplitude = 50.0;  // A
+        float frequency = 2.0;   // B (frekvencia szorzó)
+        float offset = 10.0;     // C (eltolás)
+
+        // A szinusz érték számítása az idő függvényében (count_ növekedésével)
+        message_sin.data = amplitude * sin(frequency * (count_++ / 50.0)) + offset;
+        
+        // Véletlenszám generálása
         message_rnd.data = rand() % 5;
-        //   RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message_sin.data << "'");
+
+        // Publikálás a "sine" és "rand" topicokra
         pub1_->publish(message_sin);
         pub2_->publish(message_rnd);
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_, pub2_;
     size_t count_;
