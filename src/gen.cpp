e456565668036c7591b29a4d3eeedd41e95ca6ea
cvada <gfinfgin@gmail.com>
2024-10-13 18:27:58 +0200

    modositott gen es sum fajlok feltoltese

--- src/gen.cpp
+++ src/gen.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cmath> // szükséges a sin() függvényhez
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
@@ -13,9 +14,13 @@ class GenSineAndRand : public rclcpp::Node
 public:
     GenSineAndRand(): Node("gen_node"), count_(0)
     {
-        RCLCPP_INFO(this->get_logger(), "Generating sine and random numbers");
+        RCLCPP_INFO(this->get_logger(), "Generating transformed sine and random numbers");
+        
+        // Publisher-ek létrehozása
         pub1_ = this->create_publisher<std_msgs::msg::Float32>("sine", 10);
         pub2_ = this->create_publisher<std_msgs::msg::Float32>("rand", 10);
+
+        // Timer létrehozása
         timer_ = this->create_wall_timer(50ms, std::bind(&GenSineAndRand::timer_callback, this));
     }
 
@@ -24,12 +29,23 @@ private:
     {
         auto message_sin = std_msgs::msg::Float32();
         auto message_rnd = std_msgs::msg::Float32();
-        message_sin.data = sin(count_++ / 50.0) * 100;
+        
+        // Szinusz transzformáció paraméterek
+        float amplitude = 50.0; // Amplitúdó (nagyobb/lassabb oszcilláció)
+        float frequency = 0.1;  // Frekvencia (gyorsabb/lassabb ismétlődés)
+        float phase_shift = M_PI / 4; // Fáziseltolás (pi/4 radián, azaz 45 fok)
+
+        // Transzformált szinusz érték számítása
+        message_sin.data = amplitude * sin(frequency * count_++ + phase_shift);
+
+        // Véletlenszám generálása
         message_rnd.data = rand() % 5;
-        //   RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message_sin.data << "'");
+
+        // Adatok publikálása
         pub1_->publish(message_sin);
         pub2_->publish(message_rnd);
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_, pub2_;
     size_t count_;
--- src/sum.cpp
+++ src/sum.cpp
@@ -12,49 +12,62 @@ using namespace std::chrono_literals;
 class SumData : public rclcpp::Node
 {
 public:
-    SumData(): Node("sum_node"), count_(0)
+    SumData(): Node("sum_node"), sine_(0.0), rand_(0.0), in_(0.0)
     {
         RCLCPP_INFO(this->get_logger(), "Summing numbers");
+
+        // Publisher létrehozása az "out" topicra
         pub1_ = this->create_publisher<std_msgs::msg::Float32>("out", 10);
+
+        // Subscriber-ek létrehozása a "sine", "rand" és "in" topicokra
         sub1_ = this->create_subscription<std_msgs::msg::Float32>("sine", 10, std::bind(&SumData::sine_callback, this, _1));
         sub2_ = this->create_subscription<std_msgs::msg::Float32>("rand", 10, std::bind(&SumData::rand_callback, this, _1));
         sub3_ = this->create_subscription<std_msgs::msg::Float32>("in", 10, std::bind(&SumData::in_callback, this, _1));
+
+        // Timer létrehozása 50 ms periódussal
         timer_ = this->create_wall_timer(50ms, std::bind(&SumData::timer_callback, this));
     }
 
 private:
+    // Timer callback, amely összeadja az értékeket és publikálja
     void timer_callback()
     {
         auto message_sum = std_msgs::msg::Float32();
-        message_sum.data = sine_ + rand_ + in_;
-        pub1_->publish(message_sum);
+        message_sum.data = sine_ + rand_ + in_;  // Az adatok összeadása
+        pub1_->publish(message_sum);  // Az eredmény publikálása az "out" topicra
     }
+
+    // "sine" topic callback
     void sine_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "Sine: '%f'", msg->data);
-        sine_ = msg->data;
+        sine_ = msg->data;  // A beérkező szinusz érték tárolása
     }
+
+    // "rand" topic callback
     void rand_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "Rand: '%f'", msg->data);
-        rand_ = msg->data;
+        rand_ = msg->data;  // A beérkező véletlenszám tárolása
     }
+
+    // "in" topic callback (külső adat forrás)
     void in_callback(const std_msgs::msg::Float32::SharedPtr msg)
     {
-        // RCLCPP_INFO(this->get_logger(), "In: '%f'", msg->data);
-        in_ = msg->data;
+        in_ = msg->data;  // A beérkező "in" érték tárolása
     }
+
+    // Publikáló, subscriber-ek és timer tárolása
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub1_;
     rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr sub1_, sub2_, sub3_;
-    double sum_, sine_, rand_, in_;
-    size_t count_;
+
+    // Értékek tárolása
+    double sine_, rand_, in_;
 };
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<SumData>());
+    rclcpp::spin(std::make_shared<SumData>());  // Node futtatása
     rclcpp::shutdown();
     return 0;
 }
